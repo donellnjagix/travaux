436efc0c279ba79ed16a4201470a1bf60e0d7b7f
donellnjagix <74359807+donellnjagix@users.noreply.github.com>
2023-09-12 19:21:18 +0300

    Update WhatsAppButton.tsx

--- app/components/WhatsAppButton.tsx
+++ app/components/WhatsAppButton.tsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const FloatingWButton: React.FC = () => {
   return (
     <a
-      href="https://api.whatsapp.com/send?phone=+254723942482"
+       href="https://api.whatsapp.com/send?phone=+254729304190"
       className="fixed bottom-4 right-4 bg-green-500 hover:bg-green-600 text-white py-2 px-6 rounded-full shadow-md animate-bounce flex items-center space-x-2"
       target="_blank"
       rel="noopener noreferrer"
